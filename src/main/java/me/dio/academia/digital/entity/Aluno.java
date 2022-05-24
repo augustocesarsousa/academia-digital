883c4a50020f148e5e1ce186032da60ac46033a0
Augusto Cesar <acs-03@hotmail.com>
2022-05-24 16:59:04 -0300

    Entities annotation

--- src/main/java/me/dio/academia/digital/entity/Aluno.java
+++ src/main/java/me/dio/academia/digital/entity/Aluno.java
@@ -1,21 +1,46 @@
 package me.dio.academia.digital.entity;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Aluno {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_aluno")
+public class Aluno implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String nome;
 
+  @Column(unique = true)
   private String cpf;
 
   private String bairro;
 
   private LocalDate dataDeNascimento;
 
+  @OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY)
   private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
 
 }
--- src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
+++ src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
@@ -1,17 +1,45 @@
 package me.dio.academia.digital.entity;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class AvaliacaoFisica {
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_avaliacao")
+public class AvaliacaoFisica implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "aluno_id")
   private Aluno aluno;
 
   private LocalDateTime dataDaAvaliacao = LocalDateTime.now();
 
+  @Column(name = "peso_atual")
   private double peso;
 
+  @Column(name = "altura_atual")
   private double altura;
 
 }

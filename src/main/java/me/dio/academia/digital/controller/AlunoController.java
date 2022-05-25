1e9b74194fab0bd81fac299978f4b1298637192d
Augusto Cesar <acs-03@hotmail.com>
2022-05-25 18:19:28 -0300

    Advanced query

--- src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.dio.academia.digital.entity.Aluno;
@@ -24,11 +25,6 @@ public class AlunoController {
   @Autowired
   private AlunoServiceImpl service;
 
-  @GetMapping
-  public List<Aluno> getAll() {
-    return service.getAll();
-  }
-
   @PostMapping
   public Aluno create(@Valid @RequestBody AlunoForm form) {
     return service.create(form);
@@ -37,5 +33,10 @@ public class AlunoController {
   @GetMapping("/avaliacoes/{id}")
   public List<AvaliacaoFisica> getAllAvaliacaoFisicaId(@PathVariable Long id) {
     return service.getAllAvaliacaoFisicaId(id);
+  }
+
+  @GetMapping
+  public List<Aluno> getAll(@RequestParam(value = "dataDeNascimento", required = false) String dataDeNacimento) {
+    return service.getAll(dataDeNacimento);
   }
 }
